1c8a10c577d507c7d5b05675c7a2a38c67c0749a
Felix Laniyan <flaniyanjr@gmail.com>
2023-12-13 13:18:05 -0500

    logout in navbar now

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,31 @@
 import NavBar from "./NavBar"
 import header from '../styling/header.css'
 import newlogo from '../img/newlogo.jpg'
+import {Button} from '@mui/material';
 
-function Header({user}) {
+import Signup from "./Signup";
+
+function Header({user, setUser}) {
+
+    function handleLogout() {
+        fetch('/logout', {
+          method: 'DELETE'
+        }).then((resp) => {
+          if (resp.ok) {
+            // handle logout on frontend
+            setUser(null)
+            // navigate to route, we already have it set to go back to create account page in ours
+          }
+        })
+      }
+
     
     return(
         <div>
             <NavBar />
             <div>
-            {user ? <p className="user-info"><b>Welcome: {user.username}</b></p>: null}
+                {user ? <Button varient='contained' onClick={handleLogout} id= 'logout-button'>Logout</Button> : <Signup setUser={setUser} />}
+                {user ? <p className="user-info"><b>Welcome: {user.username}</b></p>: null}
             </div>
             <div className= 'logo-container'>
                 <img id="header-img" src={newlogo} alt="pickup pal logo"/>
